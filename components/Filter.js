b292b9b140ef89b715e6e2e544dbf3082753160b
szrharrison <s.z.r.harrison@gmail.com>
2017-05-24 09:19:54 -0400

    Done.

--- components/Filter.js
+++ components/Filter.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Filter(props) {
   return (
     <select onChange={props.handleChange} defaultValue='all'>
-      <option value=''>All</option>
+      <option value='all'>All</option>
       {props.filters.map(filter =>
         <option key={filter} value={filter}>{filter}</option>
       )}
